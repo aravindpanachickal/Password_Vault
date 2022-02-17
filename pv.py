17d016b3995fd675efea5a73d8f2cefc59733ccf
Aravind_Panachickal <aravindpanachickal@gmail.com>
2022-02-17 14:11:59 +0530

    imported tkinter initialised GUI

---
+++ pv.py
@@ -0,0 +1,13 @@
+#This is a program written in python3.
+#Meant to be a Pass Word Manager
+#Author : Aravind_Panachickal
+#Project started on : 17-Feb-2022
+
+#importing modules
+import tkinter as tk
+
+#initialising GUI
+root = tk.TK()
+root.geometry('+850+1')
+root.title("Passowrd Vault")
+
