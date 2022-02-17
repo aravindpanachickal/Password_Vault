fecc159151eced06af8c63a4a1a86f4bfe674c82
Aravind_Panachickal <aravindpanachickal@gmail.com>
2022-02-17 19:11:43 +0530

    sqlite initialised

--- pv.py
+++ pv.py
@@ -5,6 +5,7 @@
 
 #importing modules
 import tkinter as tk
+import sqlite3
 
 #initialising GUI
 root = tk.Tk()
@@ -16,6 +17,26 @@ root.title("Passowrd Vault")
 #inserting icon to the window
 photo = tk.PhotoImage(file = "icon.png")
 root.iconphoto(True, photo)
+
+#initialising sql
+with sqlite3.connect('.pv_database.db') as db:
+	cursor = db.cursor()
+
+#checing and creating table for mpin
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Mpin(
+id INTEGER PRIMARY KEY,
+mpin TEXT NOT NULL,
+recoverykey TEXT NOT NULL);
+""")
+#checing and creating table for vault
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS vault(
+id INTEGER PRIMARY KEY,
+website TEXT NOT NULL,
+username TEXT NOT NULL,
+password TEXT NOT NULL);
+""")
 
 #GUI update
 root.resizable(False, False)
